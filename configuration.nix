f8d66212a59efc84832a107d142d9855377e6a40
Hugo Cornago <hugo@zuperito.xyz>
2025-10-14 15:37:17 +0000

    firewall: enable

--- configuration.nix
+++ configuration.nix
@@ -7,6 +7,7 @@
   imports = [
     ./modules/neovim/neovim.nix
     ./modules/copyparty.nix
+		./modules/cloudflare-ddns.nix
   ];
 
   boot = {
@@ -22,20 +23,22 @@
 
   networking.hostName = "server";
   networking.networkmanager.enable = true;
-  networking.firewall.enable = false;
+  networking.firewall = {
+		enable = true;
+		allowedTCPPorts = [ 22 ];
+	};
   services.openssh.enable = true;
 
   environment.systemPackages = with pkgs; [
     curl
     git
     lazygit
+		tmux
   ];
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINOHAzlWTCK89b4vehheZHX724HmclxzHnOq4RBEyF99 private"
   ];
-
-	users.groups.storage.gid = 999;
 
   system.stateVersion = "25.05";
   nix.settings.experimental-features = ["nix-command" "flakes"];
