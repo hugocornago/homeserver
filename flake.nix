af9178f03763db47fc8c7ab3276b1ae4d4342b47
Hugo <hugo@zuperito.xyz>
2026-05-09 13:51:43 +0200

    update nixpkgs 25.05 -> 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "homeserver with NixOS!";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     disko = {
       url = "github:nix-community/disko";
