02131f770ef9cda8cd36a46df47bad087374176a
Hugo Cornago <hugo@zuperito.xyz>
2026-05-18 15:25:41 +0000

    add: mosquitto

--- modules/mosquitto.nix
+++ modules/mosquitto.nix
@@ -6,32 +6,20 @@
 
   services.mosquitto = {
     enable = true;
-    logType = ["all"];
     listeners = [
       {
-        users.test = {
-          acl = [
-            "read robot/data/#"
-            "write robot/action/#"
-          ];
-          password = "testpassword";
-        };
-
         users.robot = {
           acl = [
             "write robot/data/#"
             "read robot/action/#"
           ];
-          password = "robotiscool";
-          # hashedPasswordFile = "${config.sops.secrets.mosquitto-robot-password.path}";
+          hashedPasswordFile = "${config.sops.secrets.mosquitto-robot-password.path}";
         };
 
-        users.web = {
+        users.telegram = {
           acl = [
-            "write /robot/update_scripts"
-            "write /robot/run_scripts"
-            "read /robot/odometry_update"
-            "read /robot/speed_update"
+            "read robot/data/#"
+            "write robot/action/#"
           ];
           hashedPasswordFile = "${config.sops.secrets.mosquitto-web-password.path}";
         };
@@ -39,3 +27,6 @@
     ];
   };
 }
+
+
+
